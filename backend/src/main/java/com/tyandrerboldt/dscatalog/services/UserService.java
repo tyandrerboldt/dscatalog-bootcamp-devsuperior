7d10ed4b0e6d877b50ec038f26c7423c11e9e521
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-23 16:50:12 -0300

    Implementing oAuth2 authorization server

--- backend/src/main/java/com/tyandrerboldt/dscatalog/services/UserService.java
+++ backend/src/main/java/com/tyandrerboldt/dscatalog/services/UserService.java
@@ -2,11 +2,16 @@ package com.tyandrerboldt.dscatalog.services;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +28,9 @@ import com.tyandrerboldt.dscatalog.services.exceptions.DatabaseException;
 import com.tyandrerboldt.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private UserRepository userRepository;
@@ -89,6 +96,21 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDTO.getId());
 			user.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userRepository.findByEmail(username);
+		
+		if(user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("User found: " + username);
+		
+		return user;
 	}
 	
 }
