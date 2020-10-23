25723721f509cea9dd3e022d0a52251e818b1cfc
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-23 10:08:18 -0300

    Custom constraint validator

--- backend/src/main/java/com/tyandrerboldt/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/tyandrerboldt/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.tyandrerboldt.dscatalog.dto;
 
+import com.tyandrerboldt.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
