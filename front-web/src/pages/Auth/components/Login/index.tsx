cb933949d2ddb8e651799018248d495f5c62c5a2
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-29 14:04:39 -0300

    Make login feature

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,21 +1,41 @@
 import React from 'react'
-import './styles.scss'
 import AuthCard from '../Card'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import ButtonIcon from 'core/components/ButtonIcon'
+import './styles.scss'
+import { makeLogin } from 'core/utils/request'
+
+type FormData = {
+  username: string;
+  password: string;
+}
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    makeLogin(data);
+  }
+
   return (
     <>
       <AuthCard title="Login">
-        <form className="login-form">
+        <form 
+          onSubmit={handleSubmit(onSubmit)}
+          className="login-form"
+        >
           <input 
             type="email" 
+            name="username"
+            ref={register}
             className="form-control input-base margin-bottom-30"
             placeholder="E-mail"
             />
           <input 
             type="password" 
+            name="password"
+            ref={register}
             className="form-control input-base"
             placeholder="Senha"
             />
